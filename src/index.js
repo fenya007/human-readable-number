1000aaaa6d1f8ddbd3a2b1af6f18e0089dad491c
fenya007 <59559796+fenya007@users.noreply.github.com>
2020-10-18 16:59:55 +0300

    add:task human

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function toReadable (number) {
-  
+  if (number == 0) return 'zero';
+  let result = [];
+  let allNumbers = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety'
+  }
+  if (number >= 100) {
+    let hundred = Math.floor(number / 100);
+    result.push(`${allNumbers[hundred]} hundred`);
+    number %= 100;
+  }
+
+  if (number > 19) {
+    let dozens = Math.floor(number / 10) * 10;
+    result.push(allNumbers[dozens]);
+    number %= 10;
+  }
+
+  if (number > 0) {
+    result.push(allNumbers[number]);
+  }
+return result.join(' ')
 }
